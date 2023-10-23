392e68777d1bbe85d387b250506949bcfc22516f
Julimorg <kienphongtran2003@gmail.com>
2023-10-23 22:31:51 +0700

    JobSeeker git commit -mJobSeeker
    git  add .
    git commit -mJobSeeker

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author acer
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+              int a =5 , b=6;
+              boolean  c = a < b;
+              boolean  e = a > b;
+              boolean  f = a == b;
+              boolean  g = e == c;
+              System.out.println(" c = , e=  ,f = , g = ," + c + e + f + g);
+    }
+}
